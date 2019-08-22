af30fbe295ae9dd42e28bac5ff92644ddf979861
K Hong <albtrossfacefall@gmail.com>
2019-08-22 00:43:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Add your code here
+class Dog
+  @@all=[]
+  
+  def initialize
+    @@all << 
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each {|d|puts d}
+  end
+  
+end
